35a521a2157bbd5a9b40b712856936cb05cb0597
Lrak15 <karlgks@outlook.dk>
2023-03-31 08:57:47 +0200

    healthbar demo 4.0

--- main.py
+++ main.py
@@ -17,12 +17,32 @@ print(gameWindowWidth, gameWindowHeight)
 clock = pygame.time.Clock()
 FPS = 60
 
+#player skillpoint upgrading screen
+
+
 #create two instances of the player class
-fighter_one = PlayerClass(screen, 101, 500, 100, 200)
-fighter_two = PlayerClass(screen, 1000, 700, 100, 200)
+fighter_one = PlayerClass(screen, 101, 500, 103, 200)
+fighter_two = PlayerClass(screen, 101, 102, 103, 200)
 
 #create instances of the hitbox class
 hitbox_fighterTwo_punch = HitClass(screen, 0, 0, 100, 20)
+
+health = 255
+health1 = 255
+health2 = 0
+
+# Define skillpoint upgrade degrees
+player_1_health_upgrade = 0
+player_1_strength_upgrade = 0
+player_1_speed_upgrade = 0
+player_1_knockback_upgrade = 0
+player_1_stamina_upgrade = 0
+
+player_2_health_upgrade = 0
+player_2_strength_upgrade = 0
+player_2_speed_upgrade = 0
+player_2_knockback_upgrade = 0
+player_2_stamina_upgrade = 0
 
 #load background image
 bg_img = pygame.image.load("assets/Billeder/Holstebro.jpg")
@@ -39,10 +59,77 @@ while running:
 
     for event in pygame.event.get():
         #QUIT GAME
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+            if event.key == pygame.K_h:
+
+                player_1_health_upgrade += 1
+                player_1_strength_upgrade += 1
+                player_1_speed_upgrade += 1
+                player_1_knockback_upgrade += 1
+                player_1_stamina_upgrade += 1
+
+                health -= 15
+                if health > 127.5:
+                    health2 += 30
+                else:
+                    health1 -= 30
+                    if health < 30:
+                        health1 = 255
+
         elif event.type == pygame.QUIT:
             running = False
+
+
+
+        """
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                fighter_one.moveX += fighter_one.vel
+            if event.key == pygame.K_LEFT:
+                fighter_one.moveX -= fighter_one.vel
+            if event.key == pygame.K_DOWN:
+                fighter_one.duck()
+            if event.key == pygame.K_UP:
+                if fighter_one.yPos == gameWindowHeight - fighter_one.height:
+                    fighter_one.moveY = 20
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                fighter_one.moveX -= fighter_one.vel
+            if event.key == pygame.K_LEFT:
+                fighter_one.moveX += fighter_one.vel
+            if event.key == pygame.K_DOWN:
+                fighter_one.height += fighter_one.height
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                fighter_two.moveX += fighter_two.vel
+            if event.key == pygame.K_a:
+                fighter_two.moveX -= fighter_two.vel
+            if event.key == pygame.K_s:
+                fighter_two.moveY += fighter_two.vel
+            if event.key == pygame.K_w:
+                if fighter_two.yPos == gameWindowHeight - fighter_two.height:
+                    fighter_two.moveY = 20
+            if event.key == pygame.K_SPACE:
+                fighter_two.punch_width = 200
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                fighter_two.moveX -= fighter_two.vel
+            if event.key == pygame.K_a:
+                fighter_two.moveX += fighter_two.vel
+            if event.key == pygame.K_s:
+                fighter_two.moveY -= fighter_two.vel
+            if event.key == pygame.K_w:
+                fighter_two.moveY += fighter_two.vel
+            if event.key == pygame.K_SPACE:
+                fighter_two.punch_width = 0
+            """
 
     """
     if collisionChecker(hitbox_fighterTwo_punch, fighter_one):
@@ -53,8 +140,6 @@ while running:
 
     fighter_one.move()
     fighter_one.draw()
-
-    fighter_two.draw()
 
 
     """
@@ -69,32 +154,95 @@ while running:
 
     hitbox_fighterTwo_punch.update(fighter_two.xPos+100, fighter_two.yPos + fighter_two.height/2)
     hitbox_fighterTwo_punch.draw()
+
 # Define health bar colors
-    health_bar_background_color = (100, 100, 100)
-    health_bar_outline_color = (0, 0, 0)
-    health_bar_red_color = (255, 0, 0)
-    health_bar_green_color = (0, 255, 0)
+    background_color = (100, 100, 100)
+    outline_color = (50, 50, 50)
+    black_color = (0, 0, 0)
+    health_color = (health2, health1, 0)
 
 # Define screen spacings and lengths
-    screen_spacing_1 = int(gameWindowWidth/50)
-    screen_spacing_2 = int(gameWindowWidth/200)
-    health_bar_length = int(gameWindowWidth/3)
+    screen_spacing_1 = int(gameWindowWidth / 50)
+    screen_spacing_2 = int(gameWindowWidth / 200)
+    health_bar_length = int(gameWindowWidth / 3)
 
 # Draw player 1 health bar
-    pygame.draw.rect(screen, health_bar_background_color, pygame.Rect(screen_spacing_1, screen_spacing_1, health_bar_length, 9*screen_spacing_2))
-    pygame.draw.rect(screen, health_bar_outline_color, pygame.Rect(screen_spacing_1, screen_spacing_1, health_bar_length, 9*screen_spacing_2), screen_spacing_2)
-    pygame.draw.rect(screen, health_bar_red_color, pygame.Rect(screen_spacing_1 + 2*screen_spacing_2, screen_spacing_1 + 2*screen_spacing_2, health_bar_length - 4*screen_spacing_2, 5*screen_spacing_2))
-    pygame.draw.rect(screen, health_bar_outline_color, pygame.Rect(screen_spacing_1 + 2*screen_spacing_2, screen_spacing_1 + 2*screen_spacing_2, health_bar_length - 4*screen_spacing_2, 5*screen_spacing_2), screen_spacing_2)
-    pygame.draw.rect(screen, health_bar_green_color, pygame.Rect(screen_spacing_1 + 3*screen_spacing_2, screen_spacing_1 + 3*screen_spacing_2, 300, 3*screen_spacing_2))
+    pygame.draw.rect(screen, background_color, pygame.Rect(screen_spacing_1, screen_spacing_1, health_bar_length, 9 * screen_spacing_2))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(screen_spacing_1, screen_spacing_1, health_bar_length, 9 * screen_spacing_2), screen_spacing_2)
+    pygame.draw.rect(screen, black_color, pygame.Rect(screen_spacing_1 + 2 * screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(screen_spacing_1 + 2 * screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2), screen_spacing_2)
+    pygame.draw.rect(screen, health_color, pygame.Rect(screen_spacing_1 + 3 * screen_spacing_2, screen_spacing_1 + 3 * screen_spacing_2, (health_bar_length - 6 * screen_spacing_2) * (health / 255), 3 * screen_spacing_2))
 
 # Draw player 2 health bar
-    pygame.draw.rect(screen, health_bar_background_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1, screen_spacing_1, health_bar_length, 9*screen_spacing_2))
-    pygame.draw.rect(screen, health_bar_outline_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1, screen_spacing_1, health_bar_length, 9 * screen_spacing_2), screen_spacing_2)
-    pygame.draw.rect(screen, health_bar_red_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 2*screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2))
-    pygame.draw.rect(screen, health_bar_outline_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 2 * screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2), screen_spacing_2)
-    pygame.draw.rect(screen, health_bar_green_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 3*screen_spacing_2, screen_spacing_1 + 3 * screen_spacing_2, 300, 3 * screen_spacing_2))
+    pygame.draw.rect(screen, background_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1, screen_spacing_1, health_bar_length, 9 * screen_spacing_2))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1, screen_spacing_1, health_bar_length, 9 * screen_spacing_2), screen_spacing_2)
+    pygame.draw.rect(screen, black_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 2 * screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 2 * screen_spacing_2, screen_spacing_1 + 2 * screen_spacing_2, health_bar_length - 4 * screen_spacing_2, 5 * screen_spacing_2), screen_spacing_2)
+    pygame.draw.rect(screen, health_color, pygame.Rect(gameWindowWidth - health_bar_length - screen_spacing_1 + 3 * screen_spacing_2 - (health_bar_length - 6 * screen_spacing_2) * (health / 255 - 1), screen_spacing_1 + 3 * screen_spacing_2, (health_bar_length - 6 * screen_spacing_2) / (255 / health), 3 * screen_spacing_2))
+
+# Define skillpoint upgrade colors
+    player_1_health_upgrade_color = (100 - 20 * player_1_health_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+    player_1_strength_upgrade_color = (100 - 20 * player_1_strength_upgrade, 105 + 30 * player_1_strength_upgrade, 0)
+    player_1_speed_upgrade_color = (100 - 20 * player_1_speed_upgrade, 105 + 30 * player_1_speed_upgrade, 0)
+    player_1_knockback_upgrade_color = (100 - 20 * player_1_knockback_upgrade, 105 + 30 * player_1_knockback_upgrade, 0)
+    player_1_stamina_upgrade_color = (100 - 20 * player_1_stamina_upgrade, 105 + 30 * player_1_stamina_upgrade, 0)
+
+    player_2_health_upgrade_color = (100 - 20 * player_2_health_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+    player_2_strentgh_upgrade_color = (100 - 20 * player_2_strength_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+    player_2_speed_upgrade_color = (100 - 20 * player_2_speed_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+    player_2_knockback_upgrade_color = (100 - 20 * player_2_knockback_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+    player_2_stamina_upgrade_color = (100 - 20 * player_2_stamina_upgrade, 105 + 30 * player_1_health_upgrade, 0)
+
+# Draw player 1 skillpoint upgrading screen
+    pygame.draw.rect(screen, background_color, pygame.Rect(5 * screen_spacing_1, 8 * screen_spacing_1, 11 * screen_spacing_1, 11 * screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1, 8 * screen_spacing_1, 11 * screen_spacing_1, 11 * screen_spacing_1), screen_spacing_2)
+    # Health upgrade
+    pygame.draw.rect(screen, black_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 8 * screen_spacing_1 + 2 * screen_spacing_2, 10 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, player_1_health_upgrade_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 8 * screen_spacing_1 + 2 * screen_spacing_2, player_1_health_upgrade * 2 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 8 * screen_spacing_1 + 2 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 2 * screen_spacing_1, 8 * screen_spacing_1 + 2 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 4 * screen_spacing_1, 8 * screen_spacing_1 + 2 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 6 * screen_spacing_1, 8 * screen_spacing_1 + 2 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 8 * screen_spacing_1, 8 * screen_spacing_1 + 2 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    # Strength upgrade
+    pygame.draw.rect(screen, black_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 9 * screen_spacing_1 + 3 * screen_spacing_2, 10 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, player_1_strength_upgrade_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 9 * screen_spacing_1 + 3 * screen_spacing_2, player_1_health_upgrade * 2 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 9 * screen_spacing_1 + 3 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 2 * screen_spacing_1, 9 * screen_spacing_1 + 3 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 4 * screen_spacing_1, 9 * screen_spacing_1 + 3 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 6 * screen_spacing_1, 9 * screen_spacing_1 + 3 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 8 * screen_spacing_1, 9 * screen_spacing_1 + 3 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    # Speed upgrade
+    pygame.draw.rect(screen, black_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 10 * screen_spacing_1 + 4 * screen_spacing_2, 10 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, player_1_speed_upgrade_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 10 * screen_spacing_1 + 4 * screen_spacing_2, player_1_health_upgrade * 2 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 10 * screen_spacing_1 + 4 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 2 * screen_spacing_1, 10 * screen_spacing_1 + 4 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 4 * screen_spacing_1, 10 * screen_spacing_1 + 4 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 6 * screen_spacing_1, 10 * screen_spacing_1 + 4 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 8 * screen_spacing_1, 10 * screen_spacing_1 + 4 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    # Knockback upgrade
+    pygame.draw.rect(screen, black_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 11 * screen_spacing_1 + 5 * screen_spacing_2, 10 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, player_1_knockback_upgrade_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 11 * screen_spacing_1 + 5 * screen_spacing_2, player_1_health_upgrade * 2 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 11 * screen_spacing_1 + 5 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 2 * screen_spacing_1, 11 * screen_spacing_1 + 5 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 4 * screen_spacing_1, 11 * screen_spacing_1 + 5 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 6 * screen_spacing_1, 11 * screen_spacing_1 + 5 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 8 * screen_spacing_1, 11 * screen_spacing_1 + 5 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    # Stamina upgrade
+    pygame.draw.rect(screen, black_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 12 * screen_spacing_1 + 6 * screen_spacing_2, 10 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, player_1_stamina_upgrade_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 12 * screen_spacing_1 + 6 * screen_spacing_2, player_1_health_upgrade * 2 * screen_spacing_1, screen_spacing_1))
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2, 12 * screen_spacing_1 + 6 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 2 * screen_spacing_1, 12 * screen_spacing_1 + 6 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 4 * screen_spacing_1, 12 * screen_spacing_1 + 6 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 6 * screen_spacing_1, 12 * screen_spacing_1 + 6 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+    pygame.draw.rect(screen, outline_color, pygame.Rect(5 * screen_spacing_1 + 2 * screen_spacing_2 + 8 * screen_spacing_1, 12 * screen_spacing_1 + 6 * screen_spacing_2, 2 * screen_spacing_1, screen_spacing_1), screen_spacing_2)
+
+    # Draw player 2 skillpoint upgrading screen
+
 
     pygame.event.pump()
     pygame.display.flip()
 
 pygame.quit()
+
+
